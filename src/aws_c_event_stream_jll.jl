c6b0147a1f6b0ed89750879c7594f4b0a0671617
jlbuild <juliabuildbot@gmail.com>
2025-12-10 19:00:07 +0000

    aws_c_event_stream_jll build 0.5.8+0

--- src/aws_c_event_stream_jll.jl
+++ src/aws_c_event_stream_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("aws_c_event_stream")
-JLLWrappers.@generate_main_file("aws_c_event_stream", UUID("1383d23e-2ec2-5912-9712-7b5da7cfadcb"))
+JLLWrappers.@generate_main_file("aws_c_event_stream", Base.UUID("1383d23e-2ec2-5912-9712-7b5da7cfadcb"))
 end  # module aws_c_event_stream_jll
